4883e2d0d06974e80cf472bd5d6497b2fb67a43b
Martin Ngutswen <164114946+Aonlike@users.noreply.github.com>
2025-08-18 08:13:20 +0100

    Add files via upload

---
+++ input_list.py
@@ -0,0 +1,5 @@
+input_list = [[2,3,4], [1,5,7], [4,6,8]]
+
+list_sum = input_list[[0][0] + input_list[0][1] + input_list[0][2] + 	input_list[1][0] + input_list[1][1] + input_list[1][2] + 	input_list[2][0] + input_list[2][1] + input_list[1][2]]
+
+print(list_sum)   
